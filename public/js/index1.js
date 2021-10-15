5f6476f1de07ef5d05f4689667fd67c3010035a6
vedant9877 <vedant.pawar@silo.ai>
2021-10-15 19:06:46 +0530

    number dark

--- public/js/index1.js
+++ public/js/index1.js
@@ -80,7 +80,7 @@ function nextPrev(n) {
     if (weeks<16) {
 
         var days = weeks
-        document.getElementById("dayswithus").innerHTML = "We'll help you achieve your goal in " + Math.floor(days) + " weeks!";
+        document.getElementById("dayswithus").innerHTML = "We'll help you achieve your goal in " + <strong> str(Math.floor(days))</strong> + " weeks!";
 
       }else {
 
