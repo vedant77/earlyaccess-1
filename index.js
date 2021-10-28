9dff61bbe260a6ccc04a273214b92b268e4db15f
vedant9877 <vedant.pawar@silo.ai>
2021-10-28 17:02:21 +0530

    weeks logic

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.get("/test", (req, res) => {
 
 var port  = process.env.PORT || 80;
 server.listen(443, () => console.log("Example app listening on port 4000!"));
-app.listen(port, () => console.log("Example app listening on port 4000!"));
+app.listen(80, () => console.log("Example app listening on port 4000!"));
 // app.listen(4000, () => console.log("Example app listening on port 4000!"));
 // exports.app = functions.https.onRequest(app);
 
